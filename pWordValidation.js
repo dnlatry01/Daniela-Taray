c2fcce64199040a1e5ff3ea391f2e830d0df253f
dnlatry01 <156736699+dnlatry01@users.noreply.github.com>
2024-09-21 14:14:01 +0800

    pWordValidation.js

--- pWordValidation.js
+++ pWordValidation.js
@@ -1,17 +1,40 @@
 function getMessage(){
     let username = document.getElementById("Uname").value;
     let password = document.getElementById("Pword").value;
-    alert("Hello Validation " + password);
+    
+    //expression to validate password
+    const uppercase=/[A-Z]/;
+    const lowercase=/[a-z]/;
+    const digit=/\d/;
 
-    let RegEx = /[a-z A-Z \-/{8,15}]/;
-
-    if (regEx.test(passWord)){
-        alert("valid");
+    if(username === ""){
+        alert("Please enter your username!");
+        return;
     }
-
+    if(password === ""){
+        alert("Please enter your password!");
+        return;
+    }
+    //Check if password contains at least one uppercase letter
+    if(!uppercase.test(password)){
+        alert("Password must contain at least one uppercase letter.");
+        return;
+    }
+    //Check if password contains at least one lowercase letter
+    if(!lowercase.test(password)){
+        alert("Password must contain at least one lowercase letter.");
+        return;
+    }
+    //Check if password contains at least one digit
+    if(!digit.test(password)){
+        alert("Password must contain at least one digit.");
+        return;
+    }
+    
+    alert("Hello Validation " + password);
 }
-
-
+   
+    
 document.getElementById('btnLogin').addEventListener("click" , getMessage);
 
 
